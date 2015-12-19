c7947268e6d3e7f97f0ff14bd8452b65f278e6fc
xescoder <xescoder@gmail.com>
2015-12-19 10:44:57 +0300

    add test for _getProto method

---
+++ tests/Classes._getProto.js
@@ -0,0 +1,26 @@
+describe('Classes._getProto', function() {
+
+    var Classes,
+        obj, Constructor, Proto;
+
+    before(function() {
+
+        Classes = getClasses();
+        Classes.enableTestMode();
+
+        Proto = { field: 123 };
+        Constructor = function() {};
+
+        Constructor.prototype = Proto;
+
+        obj = new Constructor();
+
+    });
+
+    it('возвращает прототип объекта', function() {
+        var proto = Classes.getProto(obj);
+        assert.strictEqual(proto.field, 123);
+        assert.strictEqual(proto, Proto);
+    });
+
+});

c8a77690b407bf93c23a85019a78fdce81549f06
xescoder <xescoder@gmail.com>
2015-12-19 21:28:55 +0300

    add _setPrivate method test

---
+++ tests/Classes._setPrivate.js
@@ -0,0 +1,15 @@
+describe('Classes._setPrivate', function() {
+
+    var Classes;
+
+    before(function() {
+        Classes = getClasses();
+        Classes.enableTestMode();
+    });
+
+    it('устанавливает значение приватного поля', function() {
+        Classes.setPrivate('testField', 123);
+        assert.strictEqual(Classes.testField, 123);
+    });
+
+});

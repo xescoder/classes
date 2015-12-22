584365bf4dbb86943408ddb36eea7b88e2cbd8b9
xescoder <xescoder@gmail.com>
2015-12-22 22:18:19 +0300

    add decl tests

---
+++ tests/Classes.decl.js
@@ -0,0 +1,30 @@
+describe('Classes.decl', function() {
+
+    var Classes;
+
+    before(function() {
+        Classes = getClasses();
+        Classes.enableTestMode();
+    });
+
+    it.only('вызывает функции для создания класса в правильном порядке', function() {
+
+        var body = {},
+            createClassSpy = sinon.spy(),
+            createPublicConstructorSpy = sinon.stub().returnsArg(0);
+
+        Classes.setPrivate('createClass', createClassSpy);
+        Classes.setPrivate('createPublicConstructor', createPublicConstructorSpy);
+
+        Classes.decl('Test', body);
+
+        assert.isTrue(createPublicConstructorSpy.calledWith(body));
+        assert.isTrue(createPublicConstructorSpy.calledOnce);
+
+        assert.isTrue(createClassSpy.calledAfter(createPublicConstructorSpy));
+        assert.isTrue(createClassSpy.calledWith('Test', body));
+        assert.isTrue(createClassSpy.calledOnce);
+
+    });
+
+});
